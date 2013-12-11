4a76049d81efe8bf945535363cf973ae84f9ddcd
jona <omnigoat@gmail.com>
2013-12-11 18:04:17 +1100

    more internal events

--- source/widgets/window.cpp
+++ source/widgets/window.cpp
@@ -1,6 +1,7 @@
 #include <fooey/widgets/window.hpp>
 
 #include <fooey/events/move.hpp>
+#include <fooey/events/resize.hpp>
 
 #include <string>
 #include <tuple>
@@ -13,9 +14,14 @@ using fooey::window_ptr;
 window_t::window_t(properties::captioned_t::caption_t const& c, uint32_t width, uint32_t height)
 	: widget_t(width, height), captioned_t(c)
 {
-	on("move.system", [&](events::move_t& e) {
+	on("move.internal", [&](events::move_t& e) {
 		left_ = e.left();
 		top_ = e.top();
+	});
+
+	on("resize.internal", [&](events::resize_t& e) {
+		width_ = e.width();
+		height_ = e.height();
 	});
 }
 

4212537628a43c523d54148134987ca6e4db01cc
jona <omnigoat@gmail.com>
2014-07-19 23:11:16 +1000

    redid key input event triggering

--- include/fooey/input/key_state.hpp
+++ include/fooey/input/key_state.hpp
@@ -7,70 +7,46 @@
 #include <functional>
 #include <bitset>
 //======================================================================
-namespace fooey {
-
+namespace fooey
+{
 	enum class key_t : uint8;
+
+
+
 
 	struct key_combination_t
 	{
 		typedef std::bitset<128> bitfield_t;
 
-		key_combination_t(key_t k)
-		{
-			keys_[static_cast<uint8_t>(k)] = true;
-		}
+		key_combination_t(key_t k);
+		key_combination_t(std::initializer_list<key_t> keys);
 
-		key_combination_t(std::initializer_list<key_t> keys)
-		{
-			for (auto const x : keys)
-				keys_[static_cast<uint8_t>(x)] = true;
-		}
-
-		auto bitfield() const -> bitfield_t const& { return keys_; }
+		auto bitfield() const -> bitfield_t const&;
 
 	private:
 		bitfield_t keys_;
 	};
 
-	inline auto operator + (key_t lhs, key_t rhs) -> key_combination_t
-	{
-		return key_combination_t{lhs, rhs};
-	}
+	auto operator + (key_t lhs, key_t rhs) -> key_combination_t;
 
-#if 0
-	inline auto operator & (key_combination_t const& lhs, key_t rhs) -> key_combination_t
-	{
-		return key_combination_t(lhs.bitfield() & static_cast<uint8_t>(rhs));
-	}
-#endif
+
+
 
 	struct key_sequence_t
 	{
 		typedef std::vector<key_combination_t> combinations_t;
 
-		key_sequence_t(key_t k)
-		{
-			combinations_.push_back(key_combination_t(k));
-		}
+		key_sequence_t(key_t);
+		key_sequence_t(std::initializer_list<key_combination_t>);
+		key_sequence_t(key_combination_t const&);
 
-		key_sequence_t(std::initializer_list<key_combination_t> combinations)
-			: combinations_(combinations.begin(), combinations.end())
-		{
-		}
-
-		key_sequence_t(key_combination_t const& c)
-		{
-			combinations_.push_back(c);
-		}
-
-		auto combination(uint32 i) const -> key_combination_t const&
-		{
-			return combinations_[i];
-		}
+		auto combination(uint i) const -> key_combination_t const&;
+		auto chord_count() const -> uint;
 
 	private:
 		combinations_t combinations_;
 	};
+
 
 
 
@@ -81,16 +57,16 @@ namespace fooey {
 		auto down(key_t) -> void;
 		auto up(key_t) -> void;
 
-		auto on_key_down(key_combination_t const&, std::function<void()> const&)->uint32;
-		auto on_key_up(key_combination_t const&, std::function<void()> const&)->uint32;
-		
+		auto on_key_down(key_sequence_t const&, std::function<void()> const&) -> uint;
+		auto on_key_up  (key_sequence_t const&, std::function<void()> const&) -> uint;
+
 	private:
 		auto perform_on_down_actions() -> void;
 		auto chord_match(bitfield_t const&) -> bool;
 		auto chord_partial_match(bitfield_t const&) -> bool;
 
 	private:
-		typedef std::tuple<key_sequence_t, std::function<void()>, uint32> event_t;
+		typedef std::tuple<key_sequence_t, std::function<void()>, uint> event_t;
 		typedef std::vector<event_t> events_t;
 
 		bitfield_t bitfield_;
--- source/input/key_state.cpp
+++ source/input/key_state.cpp
@@ -1,11 +1,68 @@
 #include <fooey/input/key_state.hpp>
 
 #include <fooey/keys.hpp>
+
 #include <algorithm>
 
-using namespace fooey;
 
+using namespace fooey;
+using fooey::key_combination_t;
+using fooey::key_sequence_t;
 using fooey::key_state_t;
+
+
+
+key_combination_t::key_combination_t(key_t k)
+{
+	keys_[static_cast<uint8_t>(k)] = true;
+}
+
+key_combination_t::key_combination_t(std::initializer_list<key_t> keys)
+{
+	for (auto const x : keys)
+		keys_[static_cast<uint8_t>(x)] = true;
+}
+
+auto key_combination_t::bitfield() const -> bitfield_t const&
+{
+	return keys_;
+}
+
+
+auto fooey::operator + (key_t lhs, key_t rhs) -> key_combination_t
+{
+	return key_combination_t{lhs, rhs};
+}
+
+
+key_sequence_t::key_sequence_t(key_t k)
+{
+	combinations_.push_back(key_combination_t(k));
+}
+
+key_sequence_t::key_sequence_t(std::initializer_list<key_combination_t> combinations)
+: combinations_(combinations.begin(), combinations.end())
+{
+}
+
+key_sequence_t::key_sequence_t(key_combination_t const& c)
+{
+	combinations_.push_back(c);
+}
+
+auto key_sequence_t::combination(uint i) const -> key_combination_t const&
+{
+	return combinations_[i];
+}
+
+auto key_sequence_t::chord_count() const -> uint
+{
+	return (uint)combinations_.size();
+}
+
+
+
+
 
 auto key_state_t::down(key_t k) -> void
 {
@@ -16,55 +73,50 @@ auto key_state_t::down(key_t k) -> void
 
 	bitfield_[ki] = true;
 
-	// find all events that use this key as a trigger
-	events_t es;
+	// trigger all events waiting upon this key combination
 	for (auto& e : down_events_)
 	{
 		key_sequence_t const& seq = std::get<0>(e);
-		uint32& chord = std::get<2>(e);
+		uint& chord = std::get<2>(e);
+		auto const& combination_bitfield = seq.combination(chord).bitfield();
 
-		auto matching_event = bitfield_ & seq.combination(chord).bitfield();
-		
-		if (matching_event.at(ki) != 0)
-		//if (chord_partial_match(seq.combination(chord).bitfield()))
+		auto matching_event = bitfield_ & combination_bitfield;
+		if (combination_bitfield.test(ki) && matching_event == combination_bitfield)
 		{
-			//if (chord_match(seq.combination(chord).bitfield()))
-				es.push_back(e);
+			++chord;
+			if (chord == seq.chord_count()) {
+				std::get<1>(e)();
+				chord = 0;
+			}
 		}
 		else {
 			chord = 0;
 		}
 	}
-
-	for (auto const& e : es)
-	{
-		std::get<1>(e)();
-	}
 }
 
 auto key_state_t::up(key_t k) -> void
 {
-	uint8_t ki = static_cast<uint8_t>(k);
+	uint8 ki = static_cast<uint8>(k);
 
-	// if we're already up, ignore
-	if (!bitfield_[ki])
+	if (!bitfield_.test(ki))
 		return;
 
-	// find all events that use this key as a trigger
-	events_t es;
+	// trigger all events waiting upon this key combination
 	for (auto& e : up_events_)
 	{
 		key_sequence_t const& seq = std::get<0>(e);
-		uint32& chord = std::get<2>(e);
+		uint& chord = std::get<2>(e);
+		auto const& combination_bitfield = seq.combination(chord).bitfield();
 
-		auto nr = seq.combination(chord).bitfield() & bitfield_;
-		//auto knr = nr
-
-		if (nr.at(ki))
-		//if (chord_partial_match(seq.combination(chord).bitfield()))
+		auto matching_event = bitfield_ & combination_bitfield;
+		if (combination_bitfield.test(ki) && matching_event == combination_bitfield)
 		{
-			//if (chord_match(seq.combination(chord).bitfield()))
-				es.push_back(e);
+			++chord;
+			if (chord == seq.chord_count()) {
+				std::get<1>(e)();
+				chord = 0;
+			}
 		}
 		else {
 			chord = 0;
@@ -72,39 +124,16 @@ auto key_state_t::up(key_t k) -> void
 	}
 
 	bitfield_[ki] = false;
-
-
-	for (auto const& e : es)
-	{
-		std::get<1>(e)();
-	}
 }
 
-auto key_state_t::chord_match(bitfield_t const& rhs) -> bool
+auto key_state_t::on_key_down(key_sequence_t const& k, std::function<void()> const& fn) -> uint
 {
-	// bits in the bitfield that are set that aren't required in rhs
-	auto nr = (bitfield_ ^ rhs) & bitfield_;
-
-	return nr.none() && (bitfield_ ^ ~rhs).all();
+	down_events_.push_back(std::make_tuple(k, fn, 0u));
+	return (uint)down_events_.size();
 }
 
-auto key_state_t::chord_partial_match(bitfield_t const& rhs) -> bool
+auto key_state_t::on_key_up(key_sequence_t const& k, std::function<void()> const& fn)->uint
 {
-	// bits in the bitfield that are set that aren't required in rhs
-	auto nr = (bitfield_ ^ rhs) & bitfield_;
-	
-	return nr.none() && (bitfield_ & rhs).any();
-}
-
-
-auto key_state_t::on_key_down(key_combination_t const& k, std::function<void()> const& fn) -> uint32
-{
-	down_events_.push_back(std::make_tuple(fooey::key_sequence_t(k), fn, 0u));
-	return (uint32)down_events_.size();
-}
-
-auto key_state_t::on_key_up(key_combination_t const& k, std::function<void()> const& fn)->uint32
-{
-	up_events_.push_back(std::make_tuple(fooey::key_sequence_t(k), fn, 0u));
-	return (uint32)up_events_.size();
+	up_events_.push_back(std::make_tuple(k, fn, 0u));
+	return (uint)up_events_.size();
 }
